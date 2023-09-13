bad304933bcf7761301288882908ff7d8c05a28a
Rocco Reus <rocco.reus@hva.nl>
2023-09-13 10:43:52 +0200

    added login page

---
+++ src/login.ts
@@ -0,0 +1,26 @@
+import "./hboictcloud-config";
+
+/**
+ * Entry point
+ */
+function app(): void {
+
+    const button: HTMLElement | null = document.querySelector(".submit");
+    button?.addEventListener("click", handleClick);
+
+    async function handleClick(this: HTMLElement): Promise<void> {
+        console.log("Clicked!");
+        this.classList.add("active");
+
+        await delay(400);
+
+        this.classList.remove("active");
+
+    }
+}
+
+function delay(ms: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+app();
