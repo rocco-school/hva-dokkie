e372c440feb4404d0e839a4c490bee577af368b6
Rocco Reus <rocco.reus@hva.nl>
2023-09-14 12:08:16 +0200

    changed up homepage styling

--- src/homepage.ts
+++ src/homepage.ts
@@ -9,6 +9,13 @@ function app(): void {
         item.addEventListener("click", handleClick);
     });
 
+    const button:Element|null = document.querySelector(".start_button");
+    button?.addEventListener("click", scrollDown);
+
+    async function scrollDown(this: HTMLElement): Promise<void> {
+        const elem:Element|null = document.querySelector(".faq-container");
+        elem?.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+    }
     async function handleClick(this: HTMLElement): Promise<void> {
 
         document.querySelectorAll(".faq").forEach(item => {
@@ -21,6 +28,7 @@ function app(): void {
         // let parent:HTMLElement|null = this.parentElement;
         // parent?.parentElement?.classList.toggle("is-active");
     }
+
 }
 
 app();
