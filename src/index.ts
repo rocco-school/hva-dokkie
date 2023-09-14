3710197522ac4a445c8f0a5337251ea98cce6918
rocco <rocco.reus@hva.nl>
2023-09-15 00:00:39 +0200

    started making homepage responsive

--- src/index.ts
+++ src/index.ts
@@ -9,18 +9,28 @@ function app(): void {
         item.addEventListener("click", handleClick);
     });
 
+    const button:Element|null = document.querySelector(".start_button");
+    button?.addEventListener("click", scrollDown);
+
+    async function scrollDown(this: HTMLElement): Promise<void> {
+        const elem:Element|null = document.querySelector(".faq-container");
+        elem?.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+    }
     async function handleClick(this: HTMLElement): Promise<void> {
 
         document.querySelectorAll(".faq").forEach(item => {
             if (item !== this) {
                 item.classList.remove("is-active");
+                item.classList.add("faq__height");
             }
         });
 
+        this.classList.toggle("faq__height");
         this.classList.toggle("is-active");
         // let parent:HTMLElement|null = this.parentElement;
         // parent?.parentElement?.classList.toggle("is-active");
     }
+
 }
 
 app();
